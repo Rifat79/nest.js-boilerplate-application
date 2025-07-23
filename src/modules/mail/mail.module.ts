01157e3e38547c3206bf517bd30756ef2074ce6c
Rifat79 <ibrahim.rifat.127700@gmail.com>
2025-07-23 08:59:40 +0600

    added try catch in mail processor

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,12 +1,14 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from 'src/common/logger-v2/logger.module';
 import { MailProcessor } from './mail.processor';
 import { MailService } from './mail.service';
 
 @Module({
   imports: [
     ConfigModule,
+    LoggerModule,
     BullModule.registerQueueAsync({
       name: 'mail-queue',
       useFactory: async (config: ConfigService) => ({
--- src/modules/mail/mail.processor.ts
+++ src/modules/mail/mail.processor.ts
@@ -6,12 +6,16 @@ import * as handlebars from 'handlebars';
 import * as nodemailer from 'nodemailer';
 import { Options, SentMessageInfo } from 'nodemailer/lib/smtp-transport';
 import * as path from 'path';
+import { LoggerService } from 'src/common/logger-v2/logger.service';
 
 @Processor('mail-queue')
 export class MailProcessor {
   private transporter: nodemailer.Transporter<SentMessageInfo, Options>;
 
-  constructor(private config: ConfigService) {
+  constructor(
+    private config: ConfigService,
+    private readonly logger: LoggerService,
+  ) {
     this.transporter = nodemailer.createTransport({
       host: this.config.get('mail.host'),
       port: this.config.get<number>('mail.port'),
@@ -24,23 +28,39 @@ export class MailProcessor {
 
   @Process('send-mail')
   async handleSendMail(job: Job) {
-    const { to, subject, html, template, context, ...rest } = job.data;
+    try {
+      const { to, subject, html, template, context, ...rest } = job.data;
 
-    let compiledHtml = html;
+      let compiledHtml = html;
 
-    if (template) {
-      const filePath = path.join(__dirname, 'templates', `${template}.hbs`);
-      const source = fs.readFileSync(filePath, 'utf8');
-      const compiled = handlebars.compile(source);
-      compiledHtml = compiled(context);
+      if (template) {
+        const filePath = path.join(__dirname, 'templates', `${template}.hbs`);
+        const source = fs.readFileSync(filePath, 'utf8');
+        const compiled = handlebars.compile(source);
+        compiledHtml = compiled(context);
+      }
+
+      await this.transporter.sendMail({
+        from: this.config.get('mail.from'),
+        to,
+        subject,
+        html: compiledHtml,
+        ...rest,
+      });
+    } catch (error) {
+      this.logger.error(
+        `Mail sending failed[${job.attemptsMade}]: `,
+        error,
+        'MailProcessor',
+        job.data,
+      );
+
+      const isFinalAttempt = job.attemptsMade >= (job.opts.attempts || 1) - 1;
+      if (isFinalAttempt) {
+        // TODO record the failed job in DB
+      }
+
+      throw error;
     }
-
-    await this.transporter.sendMail({
-      from: this.config.get('mail.from'),
-      to,
-      subject,
-      html: compiledHtml,
-      ...rest,
-    });
   }
 }
